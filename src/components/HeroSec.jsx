1fbd998916342023df04b21405464ed185776593
Fizza-Ashfaq <fizz0849@gamil.com>
2024-09-27 17:29:55 +0500

    Navbar and HeroSec

--- src/components/HeroSec.jsx
+++ src/components/HeroSec.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 
 function HeroSec() {
   return (
-    <div className='h-96 bg-gradient-to-r from-purple-500 to-pink-400'>
-      <h1>Heading</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rerum numquam praesentium vitae quasi nemo neque voluptates obcaecati illo beatae, natus amet eaque, nostrum ipsam, unde veniam earum quis? Debitis.</p>
-      <button className='border'>Shop Now</button> 
+    <div className='flex flex-wrap min-w-full w-full'>
+    <div className=' h-96 min-w-full w-full bg-gradient-to-r from-purple-500 to-pink-400 m-2'>
+      <h1 className=' ml-40 0'>Logo</h1>
+      <div className=' p-10 '>
+      <h1 className='font-bold text-xl '>Heading</h1>
+      <p className='w-30'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rerum numquam praesentium vitae quasi nemo neque voluptates obcaecati illo beatae.</p>
+      <button className='border-solid'>Shop Now</button> 
+      </div>
+    </div>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 function Navbar() {
   return (
     
-    <div className=' bg-rose-300 p-5'>
+    <div className=' p-5'>
     <ul className=' flex flex-row  pl-5 '>
             <Link to="/Home" className="m-2"><li>Home</li></Link>
             <Link to="/About" className="m-2"><li>About</li></Link>
